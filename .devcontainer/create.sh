1c140f783875b9146b3ef004ba2a4f8ba0e9a9e2
Ping Noise Man <163442704+pingnoise@users.noreply.github.com>
2024-06-18 01:33:31 +0000

    Update Devcontainer Codespace files

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -21,5 +21,12 @@ git config --global push.autoSetupRemote true
 # Update Golang for private repos
 go env -w "GOPRIVATE=github.com/ping-42/*"
 
-# Get a default set of apt stuff
-sudo apt update -qq
+mkdir -p /home/vscode/.local/bin
+
+# Install Syft for Goreleaser SBOM support
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /home/vscode/.local/bin
+
+# Install Goreleaser itself
+echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+sudo apt-get update -qq
+sudo apt-get install --no-install-recommends goreleaser
